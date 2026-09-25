159fcc271154a1fb80815a1d230680712fa92ef3
Pralove Tandukar <pralove@gmail.com>
2026-09-25 15:55:38 -0400

    Format walk and drive times again

--- app.js
+++ app.js
@@ -80,6 +80,18 @@ function todayServiceDate() {
     });
 }
 
+function formatMinutes(totalMinutes) {
+
+    const hours = Math.floor(totalMinutes / 60);
+    const minutes = totalMinutes % 60;
+
+    if (hours === 0) {
+        return `${minutes} min`;
+    }
+
+    return `${hours}h ${minutes}m`;
+}
+
 function updateBusDisplay() {
 
     const now = new Date();
